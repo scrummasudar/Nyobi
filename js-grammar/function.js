ca181a2eab7abc010df4a49a3126fb92fbe364f3
scrummasudar <kentaro.masuda227@gmail.com>
2020-03-09 17:22:44 +0900

    円の面積を求める関数を追加

--- js-grammar/function.js
+++ js-grammar/function.js
@@ -5,3 +5,10 @@ function updateParagraph() {
     document.getElementById('birth-time').innerText = '生まれてから' + seconds + '秒経過。';
 }
 setInterval(updateParagraph, 50);
+
+function areaOfCircle(radius) {
+    return radius * radius * 3.14;
+}
+document.write('<p>' + areaOfCircle(5) + '</p>');
+document.write('<p>' + areaOfCircle(10) + '</p>');
+document.write('<p>' + areaOfCircle(15) + '</p>');
