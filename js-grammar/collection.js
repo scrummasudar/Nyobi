6146862ca8d2d140960737d5a9e666fbbae86d4c
scrummasudar <kentaro.masuda227@gmail.com>
2020-03-09 13:53:06 +0900

    練習問題：ペットの名前

--- js-grammar/collection.js
+++ js-grammar/collection.js
@@ -5,3 +5,11 @@ for (var garade = 1; garade < 4; garade++) {
         document.write('<p>' + garade + '年' + classes[i] + '</p>');
     }
 }
+
+var chars = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ']
+
+for (var i = 0; i < chars.length; i++) {
+    for (var j = 0; j < chars.length; j++) {
+        document.write('<p>' + chars[i] + chars[j] + '</p>')
+    }
+}
