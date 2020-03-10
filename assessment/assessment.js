9dc58b368d4b2a02c2ac1f4cf4198b813bfa871a
scrummasudar <kentaro.masuda227@gmail.com>
2020-03-10 22:11:55 +0900

    ツイート機能の開発

--- assessment/assessment.js
+++ assessment/assessment.js
@@ -31,6 +31,32 @@ assessmentButton.onclick = () => {
     const result = assessment(userName);
     paragraph.innerText = result;
     resultDivided.appendChild(paragraph);
+
+    console.log(userName)
+
+    removeAllChildren(tweetDivided);
+    const anchor = document.createElement('a');
+    const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
+        + encodeURIComponent('あなたのいいところ')
+        + '&ref_src=twsrc%5Etfw';
+
+    anchor.setAttribute('href', hrefValue);
+    anchor.className = 'twitter-hashtag-button';
+    anchor.setAttribute('data-text', result);
+    anchor.innerText = 'Tweet #あなたのいいところ';
+
+    tweetDivided.appendChild(anchor);
+
+    // widgets.js の設定
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+    tweetDivided.appendChild(script)
+}
+
+userNameInput.onkeydown = (event) => {
+    if (event.key === 'Enter') {
+        assessmentButton.onclick();
+    }
 }
 
 const answers = [
